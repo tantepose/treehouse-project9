a809cb288fd855d513f4abae6c9d4e0449dc61af
Ole Petter B. Stokke <doktoresel@gmail.com>
2018-04-24 15:09:44 +0200

    more testing

--- src/App.js
+++ src/App.js
@@ -7,13 +7,20 @@ import Search from './Components/Search';
 
 class App extends Component {
 
-  doSearch = () => {
-    const url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ee57dd1842a1228879531fecebeb8792&tags=pingpong&per_page=16&format=json&nojsoncallback=1';
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: ['Husk å ta med voks.', 'Si fra til pappa om mamma.', 'Vær det beste du kan være.'],
+      key: apiKey
+    }
   }
 
-  state = {
-    messages: ['Husk å ta med voks.', 'Si fra til pappa om mamma.', 'Vær det beste du kan være.'],
-    key: apiKey
+  doSearch = () => {
+    console.log("hallo");
+    const url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ee57dd1842a1228879531fecebeb8792&tags=pingpong&per_page=16&format=json&nojsoncallback=1';
+    this.setState({
+      messages: ['nå er det bare en']
+    });
   }
 
   render() {
@@ -23,6 +30,7 @@ class App extends Component {
         <p>Key: {this.state.key}</p>
         <ImageList 
           messages = {this.state.messages} />
+        <button onClick={this.doSearch}>Search</button>
       </div>
     );
   }
